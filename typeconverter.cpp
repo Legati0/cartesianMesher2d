3dad76031513707d17ec3a7aed1b77464c6fd227
Legati0 <yannick.zimmermann@gmx.de>
2021-02-02 17:20:47 +0100

    coordinates may now be tuples as well

--- typeconverter.cpp
+++ typeconverter.cpp
@@ -53,23 +53,38 @@ PyObject* Py3DList_FromRectangles(std::vector<Rectangle> rects) {
 
 int Point_FromPyList(PyObject* pypoint, Point* point) {
 	// Verify that each pypoint also a list
-	if (!PyList_Check(pypoint)) {
-		PyErr_SetString(PyExc_TypeError, "must pass in list of list");
+	bool isList, isTuple;
+	isList = PyList_Check(pypoint);
+	isTuple = PyTuple_Check(pypoint);
+	if (!(isList || isTuple)) {
+		PyErr_SetString(PyExc_TypeError, "must pass in list of list or tuple");
 		return NULL;
 	}
-	if (PyList_Size(pypoint) < 2){
-		PyErr_SetString(PyExc_TypeError, "coord pair list must have size 2");
+	int size = 0;
+	if (isList)
+	    size = PyList_Size(pypoint);
+	else
+	    size = PyTuple_Size(pypoint);
+	if (size < 2) {
+		PyErr_SetString(PyExc_TypeError, "must pass in list of list of size >= 2, x and y are neccessary");
 		return NULL;
 	}
-	PyObject* xcoord = PyList_GetItem(pypoint, 0);
-	PyObject* ycoord = PyList_GetItem(pypoint, 1);
+	PyObject* xcoord;
+	PyObject* ycoord;
+	if (isList) {
+	    xcoord = PyList_GetItem(pypoint, 0);
+	    ycoord = PyList_GetItem(pypoint, 1);
+	} else {
+	    xcoord = PyTuple_GetItem(pypoint, 0);
+	    ycoord = PyTuple_GetItem(pypoint, 1);
+	}
 	// Check that xcoord is a long/double
 	if (PyLong_Check(xcoord)) {
 		point->x = (double) PyLong_AsLong(xcoord);
 	} else if (PyFloat_Check(xcoord)) {
 		point->x = PyFloat_AsDouble(xcoord);
 	} else {
-		PyErr_SetString(PyExc_TypeError, "must pass in list of list of number");
+		PyErr_SetString(PyExc_TypeError, "must pass in list of list of number (whole or floating point)");
 		return NULL;
 	}
 	// Check that ycoord is a long/double
@@ -78,7 +93,7 @@ int Point_FromPyList(PyObject* pypoint, Point* point) {
 	} else if (PyFloat_Check(ycoord)) {
 		point->y = PyFloat_AsDouble(ycoord);
 	} else {
-		PyErr_SetString(PyExc_TypeError, "must pass in list of list of number");
+		PyErr_SetString(PyExc_TypeError, "must pass in list of list of number (whole or floating point)");
 		return NULL;
 	}
 	return true;
